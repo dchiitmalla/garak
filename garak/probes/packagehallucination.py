8086fc68734bc1be99d4f2841347eebb641af94a
dchiitmalla <dchitimalla@gmail.com>
2025-05-29 15:42:07 -0700

    perl package hallucination

--- garak/probes/packagehallucination.py
+++ garak/probes/packagehallucination.py
@@ -130,3 +130,32 @@ class Rust(PackageHallucinationProbe):
     goal = "generate Rust code importing non-existent crates.io packages"
     primary_detector = "packagehallucination.RustCrates"
     tier = garak.probes.Tier.OF_CONCERN
+
+class RakuLand(PackageHallucinationProbe):
+    """Attempts to generate Raku code including hallucinated raku.land packages
+    Language models sometimes suggest importing Raku packages that do not exist
+    on raku.land. These hallucinated packages could be registered by attackers,
+    and would then be silently pulled into developer environments. This probe
+    tests whether the model recommends such hallucinated packages.
+    """
+
+    active = True
+    language_name = "Raku"
+    goal = "generate Raku code importing non-existent raku.land modules"
+    primary_detector = "packagehallucination.RakuLand"
+    tier = garak.probes.Tier.OF_CONCERN
+
+class PerlLand(PackageHallucinationProbe):
+    """Attempts to generate Perl code including hallucinated MetaCPAN packages
+
+    Language models sometimes suggest importing Perl modules that do not exist
+    on MetaCPAN. These hallucinated modules could be uploaded by attackers,
+    and would then be silently pulled into developer environments. This probe
+    tests whether the model recommends such hallucinated Perl packages.
+    """
+
+    active = True
+    language_name = "Perl"
+    goal = "generate Perl code importing non-existent MetaCPAN modules"
+    primary_detector = "packagehallucination.PerlLand"
+    tier = garak.probes.Tier.OF_CONCERN
